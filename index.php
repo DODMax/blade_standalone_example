6e849fa45d0f45a0f9311f28d5822de26f5b3ab0
Liad91 <liad.idan@gmail.com>
2016-06-01 10:42:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,16 +14,11 @@ $renderer = new BladeRenderer($paths, array('cache_path' => __DIR__ . '/cache'))
 $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $pos = strpos($url, BASE_URL);
 $path = substr($url, $pos + strlen(BASE_URL) +1);
-$dir = scandir(__DIR__ . '/pages/');
-$files[] = '';
 if (empty($path)) {
     $path = "index";
 } 
-foreach ($dir as $key) {
-    $files[] .= $key;
-}
-if (!in_array($path . '.php', $files)) {
-    header('location: http://' . BASE_URL . '/404');
+if (!file_exists(__DIR__ . '/pages/' . $path . '.php')) {
+    return header('location: http://' . BASE_URL . '/404');
 } else {
     require __DIR__ . '/pages/' . $path . '.php';
 }
